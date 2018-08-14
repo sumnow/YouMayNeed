3761ec9be0867fd83d604e52eec1d6a7056bd064
mervin <mydia@foxmail.com>
2018-08-14 11:40:05 +0800

    🐙 @ cookie.js fix bug: cookie in safri can't set by chinese

--- cookie.js
+++ cookie.js
@@ -1,21 +1,30 @@
 export function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(";");
+    var cookie = '';
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == " ") c = c.substring(1);
-        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+        if (c.indexOf(name) != -1) 
+        cookie = c.substring(name.length, c.length);
+        if(/^ecdCN(\w|%)+/.test(cookie)) {
+            cookie = decodeURI(decodeURI(cookie)).slice(5)
+        }
     }
-    return "";
+    return cookie;
 }
 
 export function setCookie(cname, cvalue, exdays) {
     var d = new Date();
+    if(/[\u4e00-\u9fa5]/.test(cname)) {
+        throw new TypeError("cookie's key can not set by chinese!")
+    }
+    if(/[\u4e00-\u9fa5]/.test(cvalue)) {
+        cvalue = encodeURI(encodeURI('ecdCN'+cvalue))
+    }
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     var expires = "expires=" + d.toUTCString();
-    // console.info(cname + "=" + cvalue + "; " + expires);
     document.cookie = cname + "=" + cvalue + "; " + expires;
-    // console.info(document.cookie);
 }
 
 export function deleteCookie(cname) {
